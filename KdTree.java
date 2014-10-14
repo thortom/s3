164e4e25ab38e40bc43146f0b353ec215178e604
Thor Tomasarson <thortomasarson@gmail.com>
2014-10-14 18:12:16 +0000

    updating KdTree.java

--- KdTree.java
+++ KdTree.java
@@ -5,31 +5,91 @@ package s3;
 import java.util.Arrays;
 
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.introcs.In;
 import edu.princeton.cs.introcs.Out;
+import edu.princeton.cs.introcs.StdOut;
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdRandom;
 
 public class KdTree {
+	private Node root;             // root of BST
+	private static enum Line {VERTICAL, HORIZONTAL};
+
+    private class Node {
+        private Node left, right;  // left and right subtrees
+        private RectHV rect;
+        private Point2D p;
+
+        public Node(Point2D p) {
+            this.p = p;
+            this.left = null;
+            this.right = null;
+        }
+    }
+	
     // construct an empty set of points
     public KdTree() {
+    	root = null;
     }
 
     // is the set empty?
     public boolean isEmpty() {
-        return false;
+        return root == null;
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return size(root);
+    }
+    
+    private int size(Node x) {
+    	if(x == null)
+    		return 0;
+    	else
+    		return 1 + size(x.left) + size(x.right);
     }
 
     // add the point p to the set (if it is not already in the set)
     public void insert(Point2D p) {
+    	root = insert(root, Line.HORIZONTAL, p);
     };
+    
+    private Node insert(Node x, Line line, Point2D p) {
+    	if(x == null) return new Node(p);
+    	if(line == Line.VERTICAL) {
+    		if		(p.x() < x.p.x()) x.left = insert(x.left, Line.HORIZONTAL, p); 		// TODO: Find nicer way for comparing the points
+    		else if	(p.x() > x.p.x()) x.right = insert(x.right, Line.HORIZONTAL, p);
+    		else x.p = p; 																// <- this is not needed
+    	}
+    	else if(line == Line.HORIZONTAL) {
+    		if		(p.x() < x.p.x()) x.left = insert(x.left, Line.VERTICAL, p);
+    		else if	(p.x() > x.p.x()) x.right = insert(x.right, Line.VERTICAL, p);
+    		else x.p = p; 																// <- this is not needed
+    	}
+    	return x;
+    }
 
     // does the set contain the point p?
     public boolean contains(Point2D p) {
-        return false;
+        return contains(root, Line.HORIZONTAL, p);
+    }
+    
+    private boolean contains(Node x, Line line, Point2D p) {
+    	
+    	if(x == null) return false;
+    	if(line == Line.VERTICAL) {
+    		if		(p.x() < x.p.x()) return contains(x.left, Line.HORIZONTAL, p); 		// TODO: Find nicer way for comparing the points
+    		else if	(p.x() > x.p.x()) return contains(x.right, Line.HORIZONTAL, p);
+    		else return true;
+    	}
+    	else if(line == Line.HORIZONTAL) {
+    		if		(p.x() < x.p.x()) return contains(x.left, Line.VERTICAL, p);
+    		else if	(p.x() > x.p.x()) return contains(x.right, Line.VERTICAL, p);
+    		else return true;
+    	}
+    	StdOut.println("Error in contains, should not be here");
+    	return false;
     }
 
     // draw all of the points to standard draw
@@ -51,6 +111,25 @@ public class KdTree {
      * Test client
      ******************************************************************************/
     public static void main(String[] args) {
+    	int N = StdIn.readInt();
+    	KdTree kdt = new KdTree();
+    	
+    	Point2D[] points = new Point2D[N];
+    	for(int i = 0; i < N; i++) {
+    		double x = StdRandom.uniform(0, 10)/10.0;
+    		double y = StdRandom.uniform(0, 10)/10.0;
+    		points[i] = new Point2D(x, y);
+    		
+    		StdOut.println("x = " + x + " y = " + y);
+    	}
+    	
+    	for(int i = 0; i < N; i++) {
+    		kdt.insert(points[i]);
+    	}
+    	for(int i = 0; i < N; i++) {
+    		StdOut.println("Contains point " + "p = " + points[i] + ", " + kdt.contains(points[i]));
+    	}
+    	/*
         In in = new In();
         Out out = new Out();
         int nrOfRecangles = in.readInt();
@@ -102,5 +181,6 @@ public class KdTree {
         }
 
         out.println();
+        */
     }
 }
